a5e418bdcffe4d08abba946b64e8effcc0f43940
sau34566-spec <sau34566@gmail.com>
2026-08-17 13:49:39 +0530

    Add Firebase configuration and initialization

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,1 +1,24 @@
-// Firebase configuration will be added in the Firebase setup step.
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+import {
+  getAuth
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+import {
+  getFirestore
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "physics-test-series-405c7.firebaseapp.com",
+  projectId: "physics-test-series-405c7",
+  storageBucket: "physics-test-series-405c7.firebasestorage.app",
+  messagingSenderId: "758620061190",
+  appId: "1:758620061190:web:dbbc7759c95b5e7d8b8d87",
+  measurementId: "G-Z5QVQMZ082"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { app, auth, db };
