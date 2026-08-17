4a17decbee5012c28f748aac32bececb9bafc16e
sau34566-spec <sau34566@gmail.com>
2026-08-17 14:43:55 +0530

    Add Firebase connection to student portal logic

--- js/student.js
+++ js/student.js
@@ -1,1 +1,3 @@
-// Student portal logic will be added here.
+// Student portal logic will be added import { auth, db } from "./firebase-config.js";
+
+console.log("Student Firebase connected:", auth, db);here.
